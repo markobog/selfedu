edcde03f9b3a5de8f488e57cc45f8205502fdf22
Marko_Bogdanovic <marko_bogdanovic@epam.com>
2023-05-17 11:09:54 +0200

    Initial commit

---
+++ src/main/java/com/epam/selfedu/SelfeduApplication.java
@@ -0,0 +1,13 @@
+package com.epam.selfedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfeduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfeduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/selfedu/SelfeduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.selfedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfeduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
